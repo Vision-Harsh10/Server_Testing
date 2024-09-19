44e071d908c20f603215664dd0b5976df070d873
harsh vardhan <harsh.vardhan.met22@itbhu.ac.in>
2024-09-19 12:33:07 +0530

    first commit

---
+++ New_server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const Database = require('./Database.js'); 
+const menu = require('./Models/menu.js'); 
+
+app.use(express.json()); // Express has in-built support for JSON parsing
+
+app.get('/Life', (req, res) => {
+  res.send('Yes, I am always here for you.');
+});
+
+// Import the router files
+const Personroutes = require('./routes/Personroutes');
+// Use the routers
+app.use('/person', Personroutes);
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
+
+// POST API -- MENU
+// app.post('/menu', async (req,res) => {
+//   try{
+//     //Assuming the request body contains the menu data
+//     const menuItemData = req.body;
+//     const menuItem = new MenuItem(menuItemData);
+//     const menu_data = await menuItem.save();
+//     console.log('menu item saved');
+//     res.status(200).json(menu_data);
+//   }
+//   catch(err){
+//     console.log(err);
+//     res.status(500).json({error :'Internal server error'});
+//   }
+// });
+
+// //GET API -- MENU
+// app.get('/menu', async (req,res) => {
+//   try{
+//     const menuItems = await MenuItem.find();
+//     console.log('Menu Data fetch Successfully');
+//     res.status(200).json(menuItems);
+//   }
+//   catch(err){
+//     console.error(err);
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// })
+
+// app.listen(3001, () => {
+//   console.log('Server is listening on port 3001');
+// });
+
+
+
+
+
---
+++ Server2.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app  = express();
+const Database = require('./Database.js'); 
+const task = require('./Models/task.js');
+
+var TaskList = task.TaskList; 
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+
+app.get('/Start' , (req,res)=>{
+    res.send('Server is Working');
+});
+
+// Post API -- TASK
+app.post('/task' , async (req,res)=>{
+    try{
+        const TaskData = req.body;
+        const CreateData = new TaskList(TaskData);
+        const response = await CreateData.save();
+        console.log('Task Saved');
+        res.status(201).json(response);
+
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error :'Internal server error'});
+    }
+})
+
+app.get('/task', async (req,res)=>{
+    try{
+        const task = await TaskList.find();
+        console.log('Task Data Fetch')
+        res.status(201).json(task);
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error :'Internal server error'});
+    }
+})
+
+app.listen(3002, () => {
+    console.log('Server is listening on port 3002');
+  });
+
+  // Never Give up!
